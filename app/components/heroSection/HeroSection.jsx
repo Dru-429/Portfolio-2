20ffd932ddf80d566b0bb717ad840dcf3360973d
Dru-429 <ect.dhruv@gmail.com>
2025-11-05 22:31:04 +0530

    Added intro and light theme

--- app/components/heroSection/HeroSection.jsx
+++ app/components/heroSection/HeroSection.jsx
@@ -12,24 +12,28 @@ const HeroSection = () => {
   return (
     <section id="home">
       <div className=" w-100% max-h-screen relative mt-40 flex flex-col items-center">
-
         {/* //Hero */}
         <div className="w-full flex flex-col items-center justify-center">
           {/* //Header */}
-        
+
           <FlipHeader />
           {/* <FlipHeader2 /> */}
-          <Image 
+          <Image
             src="/druPuppet.png"
             alt="Dru Puppet"
             height={600}
             width={300}
             className="h-[80vh] w-auto relative -top-24"
-          />  
+          />
         </div>
 
+        <p className="text-foreground/80 w-full md:w-[20%] text-lg md:text-sm selection:bg-secondary/80 selection:text-background text-left md:absolute -right-10 top-1/2">
+          I'm a frontend developer obsessed with creating smooth, modern web experiences and automating the boring stuff. I design custom websites, build Notion templates, and develop web automation tools that save you
+          time and scale your business.
+        </p>
+
         {/* //Hero Footer */}
-        <div className="w-full px-2 border-t-[1px] -mt-24 border-foreground flex justify-between items-center relative">
+        <div className="w-full px-2 border-t-[1px] mt-10 md:-mt-24 border-foreground flex justify-between items-center relative">
           <div className=" py-5 text-md text-foreground/80">
             <h3>Email:</h3>
             <p>contact.dhruvsahoo@gmail.com</p>
@@ -39,7 +43,10 @@ const HeroSection = () => {
             <Link href="https://github.com/Dru-429" className="relative ">
               <SiGithub className="text-3xl text-foreground/80 hover:text-accent" />
             </Link>
-            <Link href="https://www.linkedin.com/in/dhruvsahoo/" className="relative ">
+            <Link
+              href="https://www.linkedin.com/in/dhruvsahoo/"
+              className="relative "
+            >
               <TiSocialLinkedinCircular className="text-4xl text-foreground/80 hover:text-accent" />
             </Link>
           </div>
